fd7f8c1741c940b392e4ccd3cbad1f95b055b3dc
Laurent Thiebault <thiebault.laurent@gmail.com>
2019-12-19 11:52:20 +0100

    Updates

---
+++ precache-manifest.3f067da014ae4f36b08db3a49991aa4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ae08a6e9afa8d1ef318ad0e5337feb",
+    "url": "/sports-explorer/index.html"
+  },
+  {
+    "revision": "dc154e4ce3b777d96c7b",
+    "url": "/sports-explorer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c1e84b804bd53a54b0c2",
+    "url": "/sports-explorer/static/js/2.25da6df1.chunk.js"
+  },
+  {
+    "revision": "da2121004c2d87da283f8d4f91d0d5e0",
+    "url": "/sports-explorer/static/js/2.25da6df1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc154e4ce3b777d96c7b",
+    "url": "/sports-explorer/static/js/main.eec5e3dd.chunk.js"
+  },
+  {
+    "revision": "47d0e61b68499fd3ee36",
+    "url": "/sports-explorer/static/js/runtime-main.409b19e2.js"
+  }
+]);
