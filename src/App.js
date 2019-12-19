a98ddaaf271e9e9fed60ede546eb96a604b2f7c9
Laurent Thiebault <thiebault.laurent@gmail.com>
2019-12-19 11:37:44 +0100

    :tada: init project with react-graph-vis

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Graph from "react-graph-vis";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [graph, setGraph] = useState();
+
+  useEffect(() => {
+    fetch('https://sports.api.decathlon.com/sports')
+      .then(res => res.json())
+      .then(sports => {
+
+        let nodes = [];
+        let edges = [];
+        
+        sports.data.forEach(({id, attributes: {name: label}, relationships: {children, related}}) => {
+          nodes = [...nodes, {
+            id,
+            label,
+          }];
+
+          if (!children.data) {
+            edges = [...edges, ...children.map(sportChild => ({ from: id, to: sportChild.data.id })), ]
+          }
+
+          if (!related.data) {
+            edges = [...edges, ...related.map(sportRelated => ({ from: id, to: sportRelated.data.id })), ]
+          }
+        });
+
+        setGraph({ nodes, edges });
+      });
+  }, [])
+
+  const options = {
+    edges: {
+      color: "#000000"
+    },
+    height: "1000px"
+  };
+
+  return graph ? (
+    <Graph
+      graph={graph}
+      options={options}
+    />
+  ) : 'Loading...';
 }
 
 export default App;
